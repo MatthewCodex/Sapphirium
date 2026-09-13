28a7e70aa83aded2b3bcc8de969d105950e797fa
MatthewCodex <147332562+MatthewCodex@users.noreply.github.com>
2026-09-13 13:49:38 -0400

    c

--- scripts/serpulo/SappTurrets.js
+++ scripts/serpulo/SappTurrets.js
@@ -980,9 +980,9 @@ const redLightningColor = Color.valueOf("ff3333");
 
 bloodrageLaser.buildType = () => extend(PowerTurret.PowerTurretBuild, bloodrageLaser, {
 
-    handleBulletHit(bullet){
+    collision(bullet){
         // Always run the base wall logic so the block registers the bullet damage correctly
-        this.super$handleBulletHit(bullet);
+        this.super$collision(bullet);
         
         // 1. Double check that the bullet is from an enemy team
         
