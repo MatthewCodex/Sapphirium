8510f30309f520ae9102dcac797b61207d04d767
MatthewCodex <147332562+MatthewCodex@users.noreply.github.com>
2026-09-16 11:27:50 -0400

    c

--- scripts/OtherDrills.js
+++ scripts/OtherDrills.js
@@ -1,5 +1,6 @@
 const _SappItems = require("SappItems");
 const ruby = Vars.content.getByName(ContentType.item, "sapphirium-ruby");
+const emerald = Vars.content.getByName(ContentType.item, "sapphirium-emerald");
 
 const D_RUBY_A = extend(GenericCrafter, "d-ruby-a", {envDisabled: Env.scorching});
 const D_RUBY_B = extend(GenericCrafter, "d-ruby-b", {envDisabled: Env.scorching});
@@ -25,11 +26,11 @@ D_RUBY_C.requirements = ItemStack.with(Items.copper, 50,  Items.lead, 50,  ruby,
 D_RUBY_D.requirements = ItemStack.with(Items.copper, 100,  Items.lead, 100,  ruby, 100);
 D_RUBY_E.requirements = ItemStack.with(Items.copper, 250,  Items.lead, 250,  Items.graphite, 250,  ruby, 250);
 
-D_RUBY_A.hasPower = false
-D_RUBY_B.hasPower = false
-D_RUBY_C.hasPower = true
-D_RUBY_D.hasPower = true
-D_RUBY_E.hasPower = true
+D_RUBY_A.hasPower = false;
+D_RUBY_B.hasPower = false;
+D_RUBY_C.hasPower = true;
+D_RUBY_D.hasPower = true;
+D_RUBY_E.hasPower = true;
 
 D_RUBY_C.consumePower(1);
 D_RUBY_D.consumePower(2);
@@ -52,3 +53,57 @@ D_RUBY_B.category = Category.production;
 D_RUBY_C.category = Category.production;
 D_RUBY_D.category = Category.production;
 D_RUBY_E.category = Category.production;
+
+
+
+const D_EMERALD_A = extend(GenericCrafter, "d-emerald-a", {envDisabled: Env.scorching});
+const D_EMERALD_B = extend(GenericCrafter, "d-emerald-b", {envDisabled: Env.scorching});
+const D_EMERALD_C = extend(GenericCrafter, "d-emerald-c", {envDisabled: Env.scorching});
+const D_EMERALD_D = extend(GenericCrafter, "d-emerald-d", {envDisabled: Env.scorching});
+const D_EMERALD_E = extend(GenericCrafter, "d-emerald-e", {envDisabled: Env.scorching});
+
+D_EMERALD_A.size = 1;
+D_EMERALD_B.size = 2;
+D_EMERALD_C.size = 3;
+D_EMERALD_D.size = 4;
+D_EMERALD_E.size = 5;
+
+D_EMERALD_A.outputItems = [new ItemStack(emerald, 1)];
+D_EMERALD_B.outputItems = [new ItemStack(emerald, 1)];
+D_EMERALD_C.outputItems = [new ItemStack(emerald, 2)];
+D_EMERALD_D.outputItems = [new ItemStack(emerald, 2)];
+D_EMERALD_E.outputItems = [new ItemStack(emerald, 3)];
+
+D_EMERALD_A.requirements = ItemStack.with(Items.copper, 10,  Items.lead, 10);
+D_EMERALD_B.requirements = ItemStack.with(Items.copper, 25,  Items.lead, 25,  emerald, 25);
+D_EMERALD_C.requirements = ItemStack.with(Items.copper, 50,  Items.lead, 50,  emerald, 50);
+D_EMERALD_D.requirements = ItemStack.with(Items.copper, 100,  Items.lead, 100,  emerald, 100);
+D_EMERALD_E.requirements = ItemStack.with(Items.copper, 250,  Items.lead, 250,  Items.graphite, 250,  emerald, 250);
+
+D_EMERALD_A.hasPower = false;
+D_EMERALD_B.hasPower = false;
+D_EMERALD_C.hasPower = true;
+D_EMERALD_D.hasPower = true;
+D_EMERALD_E.hasPower = true;
+
+D_EMERALD_C.consumePower(1);
+D_EMERALD_D.consumePower(2);
+D_EMERALD_E.consumePower(3);
+
+D_EMERALD_A.itemCapacity = 20;
+D_EMERALD_B.itemCapacity = 40;
+D_EMERALD_C.itemCapacity = 60;
+D_EMERALD_D.itemCapacity = 80;
+D_EMERALD_E.itemCapacity = 100;
+
+D_EMERALD_A.craftTime = 60;
+D_EMERALD_B.craftTime = 45;
+D_EMERALD_C.craftTime = 30;
+D_EMERALD_D.craftTime = 20;
+D_EMERALD_E.craftTime = 10;
+
+D_EMERALD_A.category = Category.production;
+D_EMERALD_B.category = Category.production;
+D_EMERALD_C.category = Category.production;
+D_EMERALD_D.category = Category.production;
+D_EMERALD_E.category = Category.production;
